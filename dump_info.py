9b86efd590750a52803549ed68795323a3816adf
cuiqs <cuiqs@126.com>
2017-04-19 14:17:03 +0800

    webpage infomation

---
+++ dump_info.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# Obtain web page information 
+
+import sys ,urllib2
+
+req=urllib2.Request(sys.argv[1])
+fd=urllib2.urlopen(req)
+print "Retrieved", fd.geturl()
+info=fd.info()
+
+for key,value in info.items():
+	print "%s=%s" %(key,value)
+
